cfbb448daa78f6b6550fbb214101390357ed5af4
tthieman <travis.thieman@gmail.com>
2013-07-31 10:36:26 -0400

    Switch over to Blueprint

--- app.py
+++
@@ -1,9 +0,0 @@
-""" Glues the various daemon pieces together to avoid circular imports. """
-
-from daemon import app
-from resources import *
-from views import *
-
-app.debug = True
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3581)
--- daemon.py
+++ daemon.py
@@ -1,10 +1,10 @@
 """ Constructs the Flask instance and any extensions. """
 
-from flask import Flask
+from flask import Blueprint
 from flask.ext.restful import Api
 from flask.ext.pymongo import PyMongo
 
-app = Flask('royale')
+app = Blueprint('tactical', __name__)
 
 from config import *
 
